50c05e4b1feab9a4966d215142b6599583b021eb
Ely Xiao <xiaochunyong@gmail.com>
2022-09-26 13:25:55 +0800

    init

---
+++ src/main/java/me/ely/init/InitApplication.java
@@ -0,0 +1,57 @@
+package me.ely.init;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+public class InitApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(InitApplication.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(InitApplication.class, args);
+
+        applicationContext.addApplicationListener(new ApplicationPidFileWriter());
+
+        logApplicationStartup(applicationContext.getEnvironment());
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port", "8080");
+        String contextPath = env.getProperty("server.servlet.context-path", "/");
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            logger.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        logger.info(
+                "\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\t{}://localhost:{}{}\n\t" +
+                        "External: \t{}://{}:{}{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles()
+        );
+    }
+}
---
+++ src/test/java/me/ely/init/InitApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ely.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
